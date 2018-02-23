daab0cd88f2a047cfe9c75451768e81f2281821c
Vladimir Roncevic <vroncevic@debtux>
2018-02-23 21:04:04 +0100

    [dyndns] Updated calls from sh_util, updated README.md

--- README.md
+++ README.md
@@ -1,35 +1,32 @@
-Dynamic DNS (Bash scripts).
-================================================================================
+# Dynamic DNS (Bash scripts).
 
 The README is used to introduce the tool and provide instructions on
 how to install the tool, any machine dependencies it may have and any
 other information that should be provided before the tool is installed.
 
-INSTALLATION
+### INSTALLATION
 
 To install this tool type the following:
 
-	cp -R ~/dyndns/bin/   /root/scripts/dyndns/ver.1.0/
+```
+cp -R ~/dyndns/bin/   /root/scripts/dyndns/ver.1.0/
+cp -R ~/dyndns/conf/  /root/scripts/dyndns/ver.1.0/
+cp -R ~/dyndns/log/   /root/scripts/dyndns/ver.1.0/
+```
 
-	cp -R ~/dyndns/conf/  /root/scripts/dyndns/ver.1.0/
-
-	cp -R ~/dyndns/log/   /root/scripts/dyndns/ver.1.0/
-
-
-DEPENDENCIES
+### DEPENDENCIES
 
 This tool requires these other modules and libraries:
 
-	sh-util https://github.com/vroncevic/sh-util
+	* sh-util https://github.com/vroncevic/sh-util
 
-COPYRIGHT AND LICENCE
+### COPYRIGHT AND LICENCE
 
-Copyright (C) 2016 by https://github.com/vroncevic/dyndns
+Copyright (C) 2018 by https://vroncevic.github.io/dyndns
 
 This tool is free software; you can redistribute it and/or modify
 it under the same terms as Bash itself, either Bash version 4.2.47 or,
 at your option, any later version of Bash 4 you may have available.
 
-![alt tag](https://raw.githubusercontent.com/vroncevic/dyndns/master/bash_logo.png)
-![alt tag](https://raw.githubusercontent.com/vroncevic/dyndns/master/linux_logo.jpg)
+:sparkles:
 
--- bin/dyndns.sh
+++ bin/dyndns.sh
@@ -11,15 +11,15 @@ UTIL_VERSION=ver.1.0
 UTIL=${UTIL_ROOT}/sh_util/${UTIL_VERSION}
 UTIL_LOG=${UTIL}/log
 
-.	${UTIL}/bin/devel.sh
-.	${UTIL}/bin/usage.sh
-.	${UTIL}/bin/check_root.sh
-.	${UTIL}/bin/check_tool.sh
-.	${UTIL}/bin/logging.sh
-.	${UTIL}/bin/load_conf.sh
-.	${UTIL}/bin/load_util_conf.sh
-.	${UTIL}/bin/progress_bar.sh
-.	${UTIL}/bin/check_process.sh
+.    ${UTIL}/bin/devel.sh
+.    ${UTIL}/bin/usage.sh
+.    ${UTIL}/bin/check_root.sh
+.    ${UTIL}/bin/check_tool.sh
+.    ${UTIL}/bin/logging.sh
+.    ${UTIL}/bin/load_conf.sh
+.    ${UTIL}/bin/load_util_conf.sh
+.    ${UTIL}/bin/progress_bar.sh
+.    ${UTIL}/bin/check_process.sh
 
 DYNDNS_TOOL=dyndns
 DYNDNS_VERSION=ver.1.0
@@ -29,22 +29,22 @@ DYNDNS_UTIL_CFG=${DYNDNS_HOME}/conf/${DYNDNS_TOOL}_util.cfg
 DYNDNS_LOG=${DYNDNS_HOME}/log
 
 declare -A DYNDNS_USAGE=(
-	[USAGE_TOOL]="__${DYNDNS_TOOL}"
-	[USAGE_EX_PRE]="# Start dynamic dns client"
-	[USAGE_EX]="__${DYNDNS_TOOL}"
+    [USAGE_TOOL]="${DYNDNS_TOOL}"
+    [USAGE_EX_PRE]="# Start dynamic dns client"
+    [USAGE_EX]="${DYNDNS_TOOL}"
 )
 
 declare -A DYNDNS_LOGGING=(
-	[LOG_NAME]="${DYNDNS_TOOL}"
-	[LOG_FLAG]="info"
-	[LOG_PATH]="${DYNDNS_LOG}"
-	[LOG_MSGE]="Started ${DYNDNS_TOOL}"
+    [LOG_NAME]="${DYNDNS_TOOL}"
+    [LOG_FLAG]="info"
+    [LOG_PATH]="${DYNDNS_LOG}"
+    [LOG_MSGE]="Started ${DYNDNS_TOOL}"
 )
 
 declare -A PB_STRUCTURE=(
-	[BW]=50
-	[MP]=100
-	[SLEEP]=0.01
+    [BW]=50
+    [MP]=100
+    [SLEEP]=0.01
 )
 
 TOOL_DBG="false"
@@ -55,68 +55,68 @@ TOOL_NOTIFY="false"
 # @brief   Main function
 # @param   Value optional help
 # @exitval Function __dyndns exit with integer value
-#			0   - tool finished with success operation
-#			128 - failed to load tool script configuration from files
-#			129 - missing external tool ddclient
-#			130 - process is already running
+#            0   - tool finished with success operation
+#            128 - failed to load tool script configuration from files
+#            129 - missing external tool ddclient
+#            130 - process is already running
 #
 # @usage
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 #
 # __dyndns "help"
 #
-function __dyndns() {
-	local HELP=$1
-	if [ "${HELP}" == "help" ]; then
-		__usage DYNDNS_USAGE
-		exit 0
-	fi
-	local FUNC=${FUNCNAME[0]} MSG="None" STATUS_CONF STATUS_CONF_UTIL STATUS
-	MSG="Loading basic and util configuration!"
-	__info_debug_message "$MSG" "$FUNC" "$DYNDNS_TOOL"
-	__progress_bar PB_STRUCTURE
-	declare -A config_dyndns=()
-	__load_conf "$DYNDNS_CFG" config_dyndns
-	STATUS_CONF=$?
-	declare -A config_dyndns_util=()
-	__load_util_conf "$DYNDNS_UTIL_CFG" config_dyndns_util
-	STATUS_CONF_UTIL=$?
-	declare -A STATUS_STRUCTURE=(
-		[1]=$STATUS_CONF [2]=$STATUS_CONF_UTIL
-	)
-	__check_status STATUS_STRUCTURE
-	STATUS=$?
-	if [ $STATUS -eq $NOT_SUCCESS ]; then
-		MSG="Force exit!"
-		__info_debug_message_end "$MSG" "$FUNC" "$DYNDNS_TOOL"
-		exit 128
-	fi
-	TOOL_LOG=${config_dyndns[LOGGING]}
-	TOOL_DBG=${config_dyndns[DEBUGGING]}
-	TOOL_NOTIFY=${config_dyndns[EMAILING]}
-	local DDC=${config_dyndns_util[DDCLIENT_PATH]}
-	__check_tool "${DDC}"
-	STATUS=$?
-	if [ $STATUS -eq $SUCCESS ]; then
-		__check_process "${DDC}"
-		STATUS=$?
-		if [ $STATUS -eq $NOT_SUCCESS ]; then
-			eval "${DDC} ${config_dyndns_util[DDCLIENT_ARGS]}"
-			__info_debug_message_end "Done" "$FUNC" "$DYNDNS_TOOL"
-			exit 0
-		fi
-		MSG="Force exit!"
-		__info_debug_message_end "$MSG" "$FUNC" "$DYNDNS_TOOL"
-		exit 130
-	fi
-	MSG="Missing tool ${DDC}"
-	DYNDNS_LOGGING[LOG_MSGE]=$MSG
-	DYNDNS_LOGGING[LOG_FLAG]="error"
-	__logging DYNDNS_LOGGING
-	__sendmail "$MSG" "${configdyndns[ADMIN_EMAIL]}"
-	MSG="Force exit!"
-	__info_debug_message_end "$MSG" "$FUNC" "$DYNDNS_TOOL"
-	exit 129
+function __dyndns {
+    local HELP=$1
+    if [ "${HELP}" == "help" ]; then
+        usage DYNDNS_USAGE
+        exit 0
+    fi
+    local FUNC=${FUNCNAME[0]} MSG="None" STATUS_CONF STATUS_CONF_UTIL STATUS
+    MSG="Loading basic and util configuration!"
+    info_debug_message "$MSG" "$FUNC" "$DYNDNS_TOOL"
+    progress_bar PB_STRUCTURE
+    declare -A config_dyndns=()
+    load_conf "$DYNDNS_CFG" config_dyndns
+    STATUS_CONF=$?
+    declare -A config_dyndns_util=()
+    load_util_conf "$DYNDNS_UTIL_CFG" config_dyndns_util
+    STATUS_CONF_UTIL=$?
+    declare -A STATUS_STRUCTURE=(
+        [1]=$STATUS_CONF [2]=$STATUS_CONF_UTIL
+    )
+    check_status STATUS_STRUCTURE
+    STATUS=$?
+    if [ $STATUS -eq $NOT_SUCCESS ]; then
+        MSG="Force exit!"
+        info_debug_message_end "$MSG" "$FUNC" "$DYNDNS_TOOL"
+        exit 128
+    fi
+    TOOL_LOG=${config_dyndns[LOGGING]}
+    TOOL_DBG=${config_dyndns[DEBUGGING]}
+    TOOL_NOTIFY=${config_dyndns[EMAILING]}
+    local DDC=${config_dyndns_util[DDCLIENT_PATH]}
+    check_tool "${DDC}"
+    STATUS=$?
+    if [ $STATUS -eq $SUCCESS ]; then
+        check_process "${DDC}"
+        STATUS=$?
+        if [ $STATUS -eq $NOT_SUCCESS ]; then
+            eval "${DDC} ${config_dyndns_util[DDCLIENT_ARGS]}"
+            info_debug_message_end "Done" "$FUNC" "$DYNDNS_TOOL"
+            exit 0
+        fi
+        MSG="Force exit!"
+        info_debug_message_end "$MSG" "$FUNC" "$DYNDNS_TOOL"
+        exit 130
+    fi
+    MSG="Missing tool ${DDC}"
+    DYNDNS_LOGGING[LOG_MSGE]=$MSG
+    DYNDNS_LOGGING[LOG_FLAG]="error"
+    logging DYNDNS_LOGGING
+    sendmail "$MSG" "${configdyndns[ADMIN_EMAIL]}"
+    MSG="Force exit!"
+    info_debug_message_end "$MSG" "$FUNC" "$DYNDNS_TOOL"
+    exit 129
 }
 
 
@@ -124,17 +124,17 @@ function __dyndns() {
 # @brief   Main entry point
 # @param   Value optional help
 # @exitval Script tool atmanger exit with integer value
-#			0   - tool finished with success operation
-#			127 - run tool script as root user from cli
-#			128 - failed to load tool script configuration from files
-#			129 - missing external tool ddclient
-#			130 - process is already running
+#            0   - tool finished with success operation
+#            127 - run tool script as root user from cli
+#            128 - failed to load tool script configuration from files
+#            129 - missing external tool ddclient
+#            130 - process is already running
 #
 printf "\n%s\n%s\n\n" "${DYNDNS_TOOL} ${DYNDNS_VERSION}" "`date`"
-__check_root
+check_root
 STATUS=$?
 if [ $STATUS -eq $SUCCESS ]; then
-	__dyndns $1
+    __dyndns $1
 fi
 
 exit 127
